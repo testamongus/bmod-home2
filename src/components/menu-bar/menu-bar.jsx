b72fcfabc53df0d37a0c22a34a66564c9bcbbaef
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-05-08 12:30:37 +0200

    let's not use classnames

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -28,26 +28,26 @@ const messages = defineMessages({
 const MenuBarComponent = function (props) {
     const { intl } = props;
     return (
-        <nav className={classNames(styles.menuBar || 'menuBar', 'menuBar')}>
-            <ul className={classNames(styles.menuBar_ul || 'menuBar_ul', 'menuBar_ul')}>
-                <li className={classNames(styles.menuBar_li || 'menuBar_li', "menuBar_li")}>
+        <nav className="menu-bar">
+            <ul className="menu-bar_ul">
+                <li className="menu-bar_li">
                     <a id="IconImage" href="https://dinosaurmod.github.io/Dino-Home"><img src="https://dinosaurmod.github.io/favicon.ico" alt="Icon"/></a>
                 </li>
-                <li className={classNames(styles.menuBar_li || 'menuBar_li', 'menuBar_li')}>
+                <li className="menu-bar_li">
                     <a id="darkModeToggle"><img src={moonIcon} alt="Moon Icon"/></a>
                 </li>
-                <li className={classNames(styles.menuBar_li || 'menuBar_li', 'menuBar_li')}>
+                <li className="menu-bar_li">
                     <a href="https://dinosaurmod.github.io/editor.html">{intl ? intl.formatMessage(messages.createText) : "Create_"}</a>
                 </li>
-                <li className={classNames(styles.menuBar_li || 'menuBar_li', 'menuBar_li')}>
+                <li className="menu-bar_li">
                     <a href="/Dino-Home/examples">{intl ? intl.formatMessage(messages.examplesText) : "Examples_"}</a>
                 </li>
-                <li className={classNames(styles.menuBar_li || 'menuBar_li', 'menuBar_li')}>
+                <li className="menu-bar_li">
                     <a href="https://dinosaurmod.betteruptime.com/">{intl ? intl.formatMessage(messages.statusText) : "Status_"}</a>
                 </li>
-                <li className={classNames(styles.menuBar_li || 'menuBar_li', 'menuBar_li')}>
+                <li className="menu-bar_li">
                     <button id="menubar_searchIcon">&#128269;</button>
-                    <input type="text" className={classNames(styles.menuBar_input || 'menuBar_input', 'menuBar_input')} id="searchBar" placeholder="Search for projects on PM..."/>
+                    <input type="text" className="menu-bar_inpuz" id="searchBar" placeholder="Search for projects on PM..."/>
                 </li>
             </ul>
             <label id="icon">
