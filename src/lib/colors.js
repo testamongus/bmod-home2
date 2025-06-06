cbc927f81241e17d95afbf17e3c5b6701811dfe4
Prankapple <bnnaadcscs@gmail.com>
2025-06-06 21:26:40 +0300

    Update colors.js

--- src/lib/colors.js
+++ src/lib/colors.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     const root = document.documentElement;
-    root.style.setProperty("--motion-primary", "#80F41A")
-    root.style.setProperty("--motion-primary-menu", "#68C516")
-    root.style.setProperty("--motion-primary-dark", "#7DD100")
+    root.style.setProperty("--motion-primary", "#f1e74a")
+    root.style.setProperty("--motion-primary-menu", "#a39c27")
+    root.style.setProperty("--motion-primary-dark", "#c9bf06")
 })
