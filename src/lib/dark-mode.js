dd4d5a9f553e3cb2b743bd3f25ea8e8cccbd1995
Prankapple <bnnaadcscs@gmail.com>
2025-06-06 22:19:53 +0300

    Update dark-mode.js

--- src/lib/dark-mode.js
+++ src/lib/dark-mode.js
@@ -21,8 +21,6 @@ $(document).ready(function(){
         $('body').removeClass('dark-mode light-mode').addClass(mode);
         $('nav').removeClass('dark-mode light-mode').addClass(mode);
         $('section').removeClass('dark-mode light-mode').addClass(mode);
-        $('footer').removeClass('dark-mode light-mode').addClass(mode);
-        $('.footer-row a').removeClass('dark-mode light-mode').addClass(mode);
         $('.section-info').removeClass('dark-mode light-mode').addClass(mode);
         $('.menu-bar_languages-dropdown').removeClass('dark-mode light-mode').addClass(mode);
         $('.menu-bar_language-option').removeClass('dark-mode light-mode').addClass(mode);
