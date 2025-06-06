6e1d8269523dd58c5cbfe7fc63cc6be3ef0f0df1
Prankapple <bnnaadcscs@gmail.com>
2025-06-06 21:54:12 +0300

    Update search.js

--- src/lib/search.js
+++ src/lib/search.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     function performSearch() {
         var searchQuery = $('#menubar_searchBar').val();
         console.log('Search Query:', searchQuery);
-        window.open(`https://penguinmod.com/search?q=${searchQuery}`)
+        window.open(`https://snailshare.vercel.app/search?q=${searchQuery}`)
     }
 
     $('#menubar_searchIcon').click(function(){
